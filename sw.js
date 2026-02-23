9c4711b1d61c3b0e05cded77dbae505104208afc
Sry <123@>
2026-02-23 15:17:48 +0800

    Site updated: 2026-02-23 15:17:48

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1771785479251";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1771831060047";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
