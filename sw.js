52f9b0ea6b9a6ce8891a746061a2bcecc680b3e7
Sry <123@>
2026-03-04 11:28:04 +0800

    Site updated: 2026-03-04 11:28:03

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1772453104289";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1772594879691";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
