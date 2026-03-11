87cce1dd6f0b8ec0060e345b801539fa5d65f837
SorryYa <1184313719@qq.com>
2026-03-11 21:54:03 +0800

    Site updated: 2026-03-11 21:54:02

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1773237001477";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1773237240933";const preCache = ["/images/taichi.png","/","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
