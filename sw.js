7acb88b2a4ea50a2c8b0c6a62c21c941a1c0992a
SorryYa <1184313719@qq.com>
2026-03-11 12:38:55 +0800

    Site updated: 2026-03-11 12:38:55

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1773203440768";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1773203913453";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
